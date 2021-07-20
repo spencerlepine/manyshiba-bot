c44ad465a98bd3586bbc50116b55e29caddd1e49
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-20 10:05:50 -0700

    configue dotenv and config

---
+++ app.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+import config from './config';
+
+dotenv.config(); // access with process.env.TWITTER_API_KEY
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apiKey: '<YOUR-TWITTER-API-KEY>',
+  apiSecret: '<YOUR-TWITTER-API-SECRET>',
+  accessToken: '<YOUR-TWITTER-ACCESS-TOKEN>',
+  accessTokenSecret: '<YOUR-TWITTER-ACCESS-TOKEN-SECRET>',
+}
