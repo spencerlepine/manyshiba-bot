f1e74c25fa05a2b147c774f837a9647636d8a763
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-20 10:38:00 -0700

    add example tweet using twit

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 import twit from 'twit';
-import config from './config';
+import config from './config.js';
 
 const twitterClient = new twit(config);
+
+const postTweet = (tweetContent) => {
+  twitterClient.post(
+    'statuses/update',
+    { status: tweetContent },
+    function (err, data, response) {
+      console.log(data)
+    }
+  );
+}
+
+postTweet('https://cdn.shibe.online/shibes/fc962a6218715fd998d41fc4f551863ed45635a4.jpg')
--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-module.exports = {
+const config = {
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET,
   access_token: process.env.TWITTER_API_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET,
 }
+
+export default config;
