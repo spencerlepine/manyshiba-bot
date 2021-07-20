b63f684d890e17f7ea0bd8d5f80ee8ead3e6f977
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-20 10:54:44 -0700

    fetch random image and post

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 import twit from 'twit';
 import config from './config.js';
+import fetch from 'node-fetch';
 
 const twitterClient = new twit(config);
+
 const INTERVAL = 1000 * 60 * 60; // s, m, h
+const API_ENDPOINT = 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true';
 
 const fetchRandomImage = async () => {
-  // fetch something
+  const resultList = await fetch(API_ENDPOINT).then(res => res.json());
+  const newImage = resultList[0];
+  return newImage;
 }
 
 const postTweet = (tweetContent) => {
-  twitterClient.post(
-    'statuses/update',
-    { status: tweetContent },
-    function (err, data, response) {
-      console.log(data)
-    }
-  );
+  if (tweetContent) {
+    twitterClient.post(
+      'statuses/update',
+      { status: tweetContent },
+      function (err, data, response) {
+        console.log(data)
+      }
+    );
+  }
 }
 
-setTimeout(() => {
-  const dogImage = fetchRandomImage();
+setTimeout(async () => {
+  const dogImage = await fetchRandomImage();
   postTweet(dogImage);
 }, INTERVAL);
