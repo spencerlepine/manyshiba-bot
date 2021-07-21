e62065d030dc8f919ed535965e794cd5fc3a4aa5
spencerlepine <SPENCERLEPINE26@GMAIL.COM>
2021-07-21 10:30:59 -0700

    use media/upload before tweeting image

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import twit from 'twit';
 import config from './config.js';
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 const twitterClient = new twit(config);
 
@@ -10,22 +11,38 @@ const API_ENDPOINT = 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls
 const fetchRandomImage = async () => {
   const resultList = await fetch(API_ENDPOINT).then(res => res.json());
   const newImage = resultList[0];
-  return newImage;
+
+  var b64content = fs.readFileSync(newImage, { encoding: 'base64' })
+  return b64content;
 }
 
-const postTweet = (tweetContent) => {
+const tweetImage = (tweetContent) => {
   if (tweetContent) {
-    twitterClient.post(
-      'statuses/update',
-      { status: tweetContent },
-      function (err, data, response) {
-        console.log(data)
-      }
-    );
+    twitterClient.post('media/upload', { media_data: tweetContent }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdStr = data.media_id_string
+      var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+      twitterClient.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          // now we can reference the media and post a tweet (media will attach to the tweet)
+          var params = { media_ids: [mediaIdStr] }
+
+          twitterClient.post('statuses/update', params, function (err, data, response) {
+            console.log(data)
+          })
+        }
+      })
+    })
   }
 }
 
 setTimeout(async () => {
   const dogImage = await fetchRandomImage();
-  postTweet(dogImage);
+  tweetImage(dogImage);
 }, INTERVAL);
+
+const dogImage = await fetchRandomImage();
+tweetImage(dogImage);
